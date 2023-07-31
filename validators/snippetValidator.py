2f2f12108409369121bc10f5df4fb951f5339a6a
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-07-31 18:35:33 +0600

    snippet validation for update

--- validators/snippetValidator.py
+++ validators/snippetValidator.py
@@ -1,10 +1,11 @@
 from fastapi import HTTPException, Request, status
 
 from database import db
+from lib.data.programming_languages import programming_languages
 from models.Snippet import Snippet
 from models.User import User
 from schemas.SnippetSchema import createSnippetSchema, updateSnippetSchema
-from lib.data.programming_languages import programming_languages
+from utils.helpers import tags_arr_to_str
 
 ext_list = [lang['ext'] for lang in programming_languages]
 
@@ -77,6 +78,11 @@ def validate_new_snippet(request: Request, snippet: createSnippetSchema):
                 detail='Pass code should be 6 characters'
             )
 
+    if snippet.tags and len(snippet.tags) > 0:
+        snippet.tags = ','.join(snippet.tags)
+    else:
+        snippet.tags = None
+
     # there will be a check for theme in future
     # there will be a check for font_size in future
 
@@ -95,25 +101,40 @@ def validate_update_snippet(request: Request, id: int, update_snippet: updateSni
     if update_snippet.title is not None:
         update_snippet.title = update_snippet.title.strip()
 
-        # Check if the updated title conflicts with any other snippet
-        conflicting_snippet = db.query(Snippet).filter(
-            Snippet.title == update_snippet.title,
-            Snippet.id != existing_snippet.id
-        ).first()
-        if conflicting_snippet:
-            raise HTTPException(
-                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-                detail='Snippet with this title already exists'
-            )
+    if update_snippet.source_code is not None:
+        update_snippet.source_code = update_snippet.source_code.strip()
 
-    if update_snippet.content is not None:
-        update_snippet.content = update_snippet.content.strip()
+    if update_snippet.language is not None:
+        update_snippet.language = update_snippet.language.strip()
 
     if update_snippet.visibility is not None:
-        if update_snippet.visibility == 2:
-            check_protected_snippet(request, update_snippet)
-        else:
-            update_snippet.share_with = ''
+        if update_snippet.pass_code is None:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                detail='Pass code is mandatory'
+            )
+
+        if not update_snippet.pass_code.isalnum():
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                detail='Invalid pass code'
+            )
+
+        if len(update_snippet.pass_code) != 6:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                detail='Pass code should be 6 characters'
+            )
+
+    if update_snippet.tags is not None:
+        if len(update_snippet.tags) > 0:
+            update_snippet.tags = tags_arr_to_str(update_snippet.tags)
+
+    if update_snippet.theme is not None:
+        update_snippet.theme = update_snippet.theme.strip()
+
+    if update_snippet.font_size is not None:
+        update_snippet.font_size = update_snippet.font_size
 
     return update_snippet
 
