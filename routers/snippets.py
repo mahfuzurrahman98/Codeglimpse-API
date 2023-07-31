120811d65ad3f6dce7870bca1d44d87d2d6da0fe
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-07-31 18:35:47 +0600

    the routes are good to go

--- routers/snippets.py
+++ routers/snippets.py
@@ -2,11 +2,12 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, Request, status
 from fastapi.responses import JSONResponse
-from utils import UID
 
 from database import db
-from models.Snippet import Snippet
-from schemas.SnippetSchema import createSnippetSchema, updateSnippetSchema, privateSnippetSchema
+from models.Snippet import Snippet, get_language_name
+from schemas.SnippetSchema import (createSnippetSchema, privateSnippetSchema,
+                                   updateSnippetSchema)
+from utils import UID
 from validators.snippetValidator import (validate_delete_snippet,
                                          validate_new_snippet,
                                          validate_update_snippet)
@@ -17,16 +18,13 @@ router = APIRouter()
 # create a snippet
 @router.post('/snippets')
 def store(request: Request, snippet: Annotated[createSnippetSchema, Depends(validate_new_snippet)]):
-
     try:
         new_snippet = Snippet(
             uid=UID.generate(),
             title=snippet.title,
             source_code=snippet.source_code,
             language=snippet.language,
-            tags=','.join(snippet.tags)
-            if (snippet.tags and len(snippet.tags) > 0)
-            else None,
+            tags=snippet.tags,
             visibility=snippet.visibility,
             pass_code=snippet.pass_code if snippet.pass_code is not None else None,
             theme=snippet.theme if snippet.theme is not None else 'monokai',
@@ -50,8 +48,8 @@ def store(request: Request, snippet: Annotated[createSnippetSchema, Depends(vali
 
 
 # get only your snippets
-@router.get('/snippets')
-def index(request: Request):
+@router.get('/snippets/my')
+def get_my_snippets(request: Request):
     try:
         snippets = db.query(Snippet).filter(
             Snippet.user_id == request.state.user.get('id')
@@ -80,8 +78,8 @@ def index(request: Request):
 
 
 # get all public snippets
-@router.get('/snippets/public')
-def get_public_snippets(request: Request):
+@router.get('/snippets')
+def index(request: Request):
     try:
         snippets = db.query(Snippet).filter(
             Snippet.visibility == 1
@@ -143,9 +141,48 @@ def show(request: Request, uid: str):
         raise HTTPException(status_code=500, detail=str(e))
 
 
+# check a snippet is private or not
+@router.get('/snippets/{uid}')
+def check_private(request: Request, uid: str):
+    try:
+        snippet = db.query(Snippet).filter(
+            Snippet.uid == uid
+        ).first()
+
+        if snippet is None:
+            return JSONResponse(
+                status_code=404,
+                content={
+                    'detail': 'Snippets not found',
+                }
+            )
+
+        snippet = snippet.serialize()
+
+        private = True
+        # if the user is the owner of the snippet
+        if request.state.user.get('id') == snippet.user_id:
+            private = False
+        else:  # if the user is not the owner of the snippet
+            # if visibility = 2, then the snippet is private, else public
+            private = snippet.visibility == 2
+
+        return JSONResponse(
+            status_code=200,
+            content={
+                'detail': 'Snipppet fetched successfully',
+                'data': {
+                    'private': private
+                }
+            }
+        )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
 # get a private snippet with passcode
 @router.post('/snippets/private/{uid}')
-def show(request: Request, uid: str, pass_code: privateSnippetSchema):
+def show_private_snippet(request: Request, uid: str, pass_code: privateSnippetSchema):
     try:
         snippet = db.query(Snippet).filter(
             Snippet.uid == uid
@@ -191,26 +228,34 @@ def update(request: Request, id: int, snippet: Annotated[updateSnippetSchema, De
 
     # Update the snippet attributes based on the request
     _snippet.title = snippet.title if snippet.title is not None else _snippet.title
-    _snippet.content = snippet.content if snippet.content is not None else _snippet.content
+    _snippet.source_code = snippet.source_code if snippet.source_code is not None else _snippet.source_code
     _snippet.language = snippet.language if snippet.language is not None else _snippet.language
     _snippet.visibility = snippet.visibility if snippet.visibility is not None else _snippet.visibility
-    _snippet.share_with = snippet.share_with if snippet.visibility == 2 else None
+    _snippet.pass_code = snippet.pass_code if snippet.pass_code is not None else _snippet.pass_code
+    _snippet.theme = snippet.theme if snippet.theme is not None else _snippet.theme
+    _snippet.font_size = snippet.font_size if snippet.font_size is not None else _snippet.font_size
 
     try:
         db.commit()
-        return {
-            'detail': 'Snippet updated successfully',
-            'data': {
-                'snippet': {
-                    'id': _snippet.id,
-                    'uid': _snippet.uid,
-                    'title': _snippet.title,
-                    'content': _snippet.content,
-                    'visibility': _snippet.visibility,
-                    'language': _snippet.programming_language.name,
+        return JSONResponse(
+            status_code=200,
+            content={
+                'detail': 'Snippet updated successfully',
+                'data': {
+                    'snippet': {
+                        'id': _snippet.id,
+                        'uid': _snippet.uid,
+                        'title': _snippet.title,
+                        'source_code': _snippet.source_code,
+                        'visibility': _snippet.visibility,
+                        'language': get_language_name(_snippet.language),
+                        'theme': _snippet.theme,
+                        'font_size': _snippet.font_size
+                    }
                 }
             }
-        }
+        )
+
     except Exception as e:
         print(e)
         raise HTTPException(status_code=500, detail=str(e))
@@ -223,7 +268,7 @@ def destroy(request: Request, snippet: Snippet = Depends(validate_delete_snippet
         db.commit()
 
         return JSONResponse(
-            status_code=200,
+            status_code=204,
             content={
                 'detail': 'Snippet deleted successfully',
             }
