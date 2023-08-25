df417edbaba6602aa902fb41663194e918bd2b2d
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-08-25 21:48:22 +0600

    setup for postgress

--- database.py
+++ database.py
@@ -7,13 +7,19 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
+db_connection = environ.get('DB_CONNECTION')
 db_host = environ.get('DB_HOST')
 db_port = environ.get('DB_PORT')
 db_name = environ.get('DB_NAME')
 db_user = environ.get('DB_USERNAME')
 db_password = environ.get('DB_PASSWORD')
+db_connector = 'mysql+mysqlconnector' if db_connection == 'mysql' else 'postgresql+psycopg2'
 
-connection_string = f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}/{db_name}"
+# connection_string = f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}/{db_name}"
+# connection_string = "postgresql://default:kwKXiHl51CvQ@ep-black-smoke-56527059.ap-southeast-1.postgres.vercel-storage.com:5432/verceldb"
+
+connection_string = f"{db_connector}://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
 # print(connection_string)
 engine = create_engine(connection_string, echo=False)
 
