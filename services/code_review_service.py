92e5371ed172cd28659401eee8f5eb6624f9b635
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2023-10-06 01:24:56 +0600

    a new service added for reviewing code

---
+++ services/code_review_service.py
@@ -0,0 +1,33 @@
+import openai
+from os import environ
+
+# Load your API key from an environment variable or secret management service
+openai.api_key = environ.get('OPENAI_API_KEY')
+
+
+def review_code(source_code):
+    try:
+        # Create a list to store all the messages for context
+        messages = [
+            {'role': 'system', 'content': 'You are a helpful assistant.'},
+        ]
+
+        # Add the source_code to the list
+        messages.append({'role': 'user', 'content': source_code})
+
+        # Add the prompt to modify the code
+        messages.append({'role': 'system', 'content': 'Do a in depth proper code review, make the code documented, and put better comments where needed. Just give the code as output, No additional messages like "here is your code, Sure, blah blah".'})
+
+        # Request gpt-3.5-turbo for chat completion
+        response = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            messages=messages
+        )
+
+        # Print the response and add it to the messages list
+        chat_message = response['choices'][0]['message']['content']
+        messages.append({'role': 'assistant', 'content': chat_message})
+        return chat_message
+
+    except Exception as e:
+        raise e
