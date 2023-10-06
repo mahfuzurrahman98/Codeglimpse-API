5de8568174949ed0a21358b70b5f4447dbc2072b
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2023-10-06 20:57:15 +0600

    call a 4th party service to review the code

--- services/code_review_service.py
+++ services/code_review_service.py
@@ -1,5 +1,8 @@
 import openai
 from os import environ
+import json
+
+import requests
 
 # Load your API key from an environment variable or secret management service
 openai.api_key = environ.get('OPENAI_API_KEY')
@@ -7,28 +10,26 @@ openai.api_key = environ.get('OPENAI_API_KEY')
 
 def review_code(source_code):
     try:
-        # Create a list to store all the messages for context
-        messages = [
-            {'role': 'system', 'content': 'You are a helpful assistant.'},
-        ]
+        service_url = environ.get('CODE_REVIEW_SERVICE_URL')
+        # send a post request to the code review service with the source_code, endpoint is /review-code
+        payload = json.dumps({
+            "source_code": source_code
+        })
+        headers = {
+            'Content-Type': 'application/json'
+        }
 
-        # Add the source_code to the list
-        messages.append({'role': 'user', 'content': source_code})
-
-        # Add the prompt to modify the code
-        messages.append({'role': 'system', 'content': 'Do a in depth proper code review, make the code documented, and put better comments where needed. Just give the code as output, No additional messages like "here is your code, Sure, blah blah".'})
-
-        # Request gpt-3.5-turbo for chat completion
-        response = openai.ChatCompletion.create(
-            model='gpt-3.5-turbo',
-            messages=messages
+        response = requests.request(
+            "POST",
+            service_url + '/review-code',
+            headers=headers, data=payload
         )
 
-        # Print the response and add it to the messages list
-        # chat_message = response['choices'][0]['message']['content']
-        # messages.append({'role': 'assistant', 'content': chat_message})
-        # return chat_message
-        return environ.get('OPENAI_API_KEY')
+        # it will return a json with {message, and data}, just take the data and return it
+        response_text = json.loads(response.text)
+        # print(response_text)
+        return response_text['data']
 
     except Exception as e:
+        # print(e)
         raise e
