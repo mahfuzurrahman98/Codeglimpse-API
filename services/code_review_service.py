b79b0071b6360ffdbb33d1396ff39ad5088e7cd2
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2023-10-07 21:14:13 +0600

    calling openai api right into the app

--- services/code_review_service.py
+++ services/code_review_service.py
@@ -8,7 +8,7 @@ import requests
 openai.api_key = environ.get('OPENAI_API_KEY')
 
 
-def review_code(source_code):
+def review_code0(source_code):
     try:
         service_url = environ.get('CODE_REVIEW_SERVICE_URL')
         # send a post request to the code review service with the source_code, endpoint is /review-code
@@ -34,3 +34,30 @@ def review_code(source_code):
     except Exception as e:
         # print(e)
         raise e
+
+def review_code(source_code):
+        openai.api_key = environ.get('OPENAI_API_KEY')
+        # Create a list to store all the messages for context
+        messages = [
+            {'role': 'system', 'content': 'You are a helpful assistant.'}
+        ]
+
+        # Add the source_code to the list
+        messages.append({'role': 'user', 'content': source_code})
+
+        # Add the prompt to modify the code
+        messages.append({'role': 'system', 'content': 'Do a in depth proper code review, make the code documented, and put better comments where needed. Just give the code as output, No additional messages like "here is your code, Sure, blah blah".'})
+
+        # Request gpt-3.5-turbo for chat completion
+        response = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            messages=messages
+        )
+
+        # Print the response and add it to the messages list
+        chat_message = response['choices'][0]['message']['content']
+        # just keep the portion between ``` and ```, and also remove the first line
+        chat_message = chat_message.split('```')[1].split('```')[0].split('\n', 1)[1]
+        
+        messages.append({'role': 'assistant', 'content': chat_message})
+        return chat_message
