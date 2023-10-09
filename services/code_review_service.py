598b848a399225241058e9561f08861d855f6a91
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2023-10-09 20:28:58 +0600

    service created to review snippet

--- services/code_review_service.py
+++ services/code_review_service.py
@@ -1,60 +1,41 @@
 import openai
 from os import environ
-import json
-
-import requests
+from fastapi import FastAPI, HTTPException
 
 # Load your API key from an environment variable or secret management service
 openai.api_key = environ.get('OPENAI_API_KEY')
 
 
-def review_code0(source_code):
+def get_response_openai(source_code, language):
     try:
-        service_url = environ.get('CODE_REVIEW_SERVICE_URL')
-        # send a post request to the code review service with the source_code, endpoint is /review-code
-        payload = json.dumps({
-            "source_code": source_code
-        })
-        headers = {
-            'Content-Type': 'application/json'
-        }
+        instructions = 'Do an in-depth code review, and improve comments, no additionl documentaion after or bvefore the code, just rewrite the code precisely.'
 
-        response = requests.request(
-            "POST",
-            service_url + '/review-code',
-            headers=headers,
-            data=payload
+        messages = [
+            {'role': 'system', 'content': 'You are an experienced software engineer reviewing a random code snippet.'},
+            {'role': 'system', 'content': f'Code snippet is in {language}.'},
+            {'role': 'user', 'content': source_code},
+            {'role': 'user', 'content': instructions}
+        ]
+        response = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo-0613',
+            messages=messages,
+            stream=True
         )
-
-        # it will return a json with {message, and data}, just take the data and return it
-        response_text = json.loads(response.text)
-        # print(response_text)
-        return response_text['data']
-
     except Exception as e:
-        # print(e)
-        raise e
+        print("Error in creating campaigns from OpenAI:", str(e))
+        raise HTTPException(503, detail=str(e))
 
-def review_code(source_code):
-    openai.api_key = environ.get('OPENAI_API_KEY')
-    # Create a list to store all the messages for context
-    messages = []
+    for chunk in response:
+        choice_delta = chunk["choices"][0]["delta"]
+        # basically the choice_delta is a object = {'content': 'something'}, if the object is not empty and has conent and the content is not empty, then we can return the content
 
-    # Add the source_code to the list
-    messages.append({'role': 'system', 'content': source_code})
-
-    # Add the prompt to modify the code
-    messages.append({'role': 'system', 'content': 'Do a in depth proper code review, make the code documented, and put better comments where needed. Just give the code as output, No additional messages like "here is your code, Sure, blah blah".'})
-
-    # Request gpt-3.5-turbo for chat completion
-    response = openai.ChatCompletion.create(
-        model='gpt-3.5-turbo',
-        messages=messages
-    )
-
-    # Print the response and add it to the messages list
-    chat_message = response['choices'][0]['message']['content']
-    # just keep the portion between ``` and ```, and also remove the first line
-    chat_message = chat_message.split('```')[1].split('```')[0].split('\n', 1)[1]
-
-    return chat_message
+        if choice_delta and choice_delta["content"]:
+            current_content = choice_delta.get("content", "")
+            # print(current_content)
+            yield current_content
+        else:
+            break
+        # replace \n with <br> to display in html
+        # current_content = current_content.replace("\n", "<br>")
+        # yield current_content
+        # print(current_content)
